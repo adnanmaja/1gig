86d35daaa7325414d7672cdd9d987c6e27367581
Rafa Adnan <rafaadnan46@gmail.com>
2025-06-12 18:52:54 +0700

    Sort

--- data_handler.py
+++ data_handler.py
@@ -3,11 +3,25 @@ import numpy as np
 import json
 file_path = "D:/Python/gig1/mhs.json"
 
-def read_jsontopd():
-    with open(file_path, "r") as file:
+with open(file_path, "r") as file:
         data = json.load(file)
         df = pd.DataFrame(data)
-        ipk_rank = df.sort_values(by='ipk', ascending=False)
+
+def ipk_rankdesc():
+    with open(file_path, "r") as file:
         print(ipk_rank)
 
-read_jsontopd()
+
+def ipk_rankasc():
+        ipk_rank = df.sort_values(by='ipk', ascending=True)
+        print(ipk_rank)
+
+def ipk_mean():
+        ipk_cumsum = df[['ipk']].mean()
+        print(ipk_cumsum)
+
+def ipk_fakmean():
+        ipk_meanfak = df.groupby("fakultas")["ipk"].mean()
+        print(ipk_meanfak)
+        
+ipk_rankasc()
